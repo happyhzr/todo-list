4e582a3b6df9b8f5bf36eb1efe5f2952cbb07e9d
Happy <insisthzr@163.com>
2021-11-05 16:45:59 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todoItems: [
+        { id: 0, value: "React", isDone: false, isSelete: false },
+      ]
+    }
+    this.inputRef = React.createRef()
+    this.addTodoItem = this.addTodoItem.bind(this)
+  }
+
+  addTodoItem() {
+    const newTodoItem = {
+      id: this.state.todoItems.length,
+      value: this.inputRef.current.value,
+      isDone: false,
+      isDelete: false,
+    }
+    this.setState({
+      todoItems: [...this.state.todoItems, newTodoItem]
+    })
+    this.inputRef.current.value = ""
+  }
+
+  deleteTodoItem(item) {
+    return () => {
+      item.isDelete = true
+      this.setState({
+        todoItems: [...this.state.todoItems, item]
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>TodoList</h1>
+        <div>
+          <input type="text" placeholder="add something..." ref={this.inputRef} />
+          <button type="submit" onClick={this.addTodoItem}>add</button>
+        </div>
+        <ul>
+          {
+            this.state.todoItems.map((item) => {
+              if (!item.isDelete) {
+                return (
+                  <li key={item.id}>
+                    <label>{item.value}</label>
+                    <button onClick={this.deleteTodoItem(item)}>delete</button>
+                  </li>
+                )
+              }
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
 }
-
-export default App;
